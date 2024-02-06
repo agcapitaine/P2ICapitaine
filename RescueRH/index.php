a79709ee5c0a318dc2cc6f90a2ca60d0b3a9f13a
agcapitaine <agcapitaine@ensc.fr>
2024-02-06 18:16:29 +0100

    update

--- RescueRH/index.php
+++ RescueRH/index.php
@@ -1,4 +1,19 @@
-<h2>Page d'accueil admin</h2>
+<?php
+require_once('include/head.php');
+?>
 
-<a href="ListeEmployes.php">Données des employés</a>
-<a href="Gestion.php">Gestion</a>
+<body>
+    
+
+<div class="container">
+<main class="mt-5 pt-4">
+<h2>Page d'accueil administrateur</h2>
+<br>
+<button type="button" class="btn btn-dark"><a href="ListeEmployes.php" class = "a">Données des employés</a></button>
+<br> <br>
+<button type="button" class="btn btn-dark"><a href="Gestion.php">Gestion</a></button>
+
+</main>
+</div>
+
+</body>
