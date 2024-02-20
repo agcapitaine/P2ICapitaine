e6d2a938b4cd66656e60fb4b7dd49bc3071901c2
agcapitaine <agcapitaine@ensc.fr>
2024-02-20 12:16:12 +0100

    presentation donnee employe

--- RescueRH/DonneeEmploye.php
+++ RescueRH/DonneeEmploye.php
@@ -22,16 +22,49 @@ $employe = $req->fetch(); // Access first (and only) result line
 
 <h2>Donnée d'un employé</h2>
 
-<h2>
-    <?= $employe['statut'] ?>
-</h2>
+<br>
 
-<p>heures effectuées ce mois-ci</p>
-<p>heures effectuées cette année</p>
-<p>objectifs de l'annualisation des heures</p>
-<p>liste des congés déjà prit par l'employé</p>
-<p>liste des congés auxquels droit</p>
-<a href="AjouterConge.php">Ajouter un congé</a>
+
+<h3>
+    <?= $employe['nom'] ?>
+</h3>
+<h3>
+    <?= $employe['prenom'] ?>
+</h3>
+<br>
+<br>
+<section style="background-color: #eee;">
+        <div class="container py-5">
+                <div class="row justify-content-center mb-3">
+                    <div class="col-md-12 col-xl-10">
+                    <div class="card shadow-0 border rounded-3">
+                    <div class="card-body">
+                    <div class="row">
+                    <div class="col-md-12 col-lg-3 col-xl-3 mb-4 mb-lg-0">
+                        <h4>Heures effectuées ce mois-ci :</h4>
+                        <p>heures effectuées ce mois-ci</p>
+                        <h4>Heures effectuées cette année :</h4>
+                        <p>heures effectuées cette année</p>
+                        <h4>Objectifs de l'annualisation des heures :</h4>
+                        <p>objectifs de l'annualisation des heures</p>
+                    </div>
+<!-- div -->
+                    <div class="col-md-6 col-lg-6 col-xl-6">
+                        <p>liste des congés déjà prit par l'employé</p>
+                        <p>liste des congés auxquels droit</p>
+                        <a href="AjouterConge.php">Ajouter un congé</a>
+                    </div>
+                    </div>
+                    </div>
+                    </div>
+                    </div>
+                </div>
+        </div>
+</section>
+
+
+
+
 
 </main>
 </div>
