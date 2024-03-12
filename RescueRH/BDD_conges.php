ee44de4f191b3ceb7fc9ef80918a9c44f07a5fb4
agcapitaine <agcapitaine@ensc.fr>
2024-03-12 18:43:17 +0100

    tentative congés avec contraintes (ne marche pas)

--- RescueRH/BDD_conges.php
+++ RescueRH/BDD_conges.php
@@ -5,6 +5,18 @@ $pageTitle = "Vérification ajout congé";
 require_once('include/head.php');
 $BDD = openDB();
 $reussi = false;
+$AleDroit= false;
+
+//compte le nombre de congé restant (dans le cas d'un type de congé précis (congé concerné))
+function nombreCongesRestant($congesdejapris, $congedroit){
+    $nbcongesconcerne = $congedroit['nbJours'];  
+    foreach ($congesdejapris as $conge) { 
+        if ($conge['titreConge']==$congedroit['titreConge']){
+            $nbcongesconcerne=$nbcongesconcerne-1; 
+        }       
+    }  
+    return $nbcongesconcerne; 
+}
 ?>
 
 <!DOCTYPE html>
@@ -12,17 +24,46 @@ $reussi = false;
 
 <body>
     <?php
+
+    
+
     if (!empty($_POST['nom']) && !empty($_POST['date']) && !empty($_POST['raison'])) {
         //on vérifie les saisies utilisateurs avec la fonction escape avant de les enregistrer dans des variables
         $nom = escape($_POST['nom']);
         $date = escape($_POST['date']);
         $raison = escape($_POST['raison']);
 
-        //on insère les données dans la table membre
-        $req = "INSERT INTO congeemploye (idUtilisateur, titreConge, dateConge) VALUES (?,?,?)";
-        $reponse = openDB()->prepare($req);
-        $reponse->execute(array($nom, $raison, $date));
-        $reussi = true;
+        //on recup l'employé
+        $req2 = openDb()->prepare('select * from utilisateur where idUtilisateur=?');
+        $req2->execute(array($nom));
+        $employe = $req2->fetch(); // Access first (and only) result line
+
+        //compte le nombre de conges auxquels l'employé a droit
+        $statutEmploye = $employe['statut'];
+        $congesDroit = openDb()->prepare('select * from conge where statutConcerne=?');
+        $congesDroit->execute(array($statutEmploye));
+
+        //compte le nombre de conges que l'employé a pris
+        $congesdejapris = openDb()->prepare('select * from congeemploye where idUtilisateur=?');
+        $congesdejapris->execute(array($nom));
+        $nbconges = $congesdejapris->rowCount();
+
+        foreach ($congesDroit as $congedroit) {
+            if (($congedroit['titreConge']==$raison) && (nombreCongesRestant($congesdejapris, $congedroit)!=1)){
+                    $AleDroit=true;
+                    echo nombreCongesRestant($congesdejapris, $congedroit);
+            }
+
+        }
+
+        if ($AleDroit==true)
+        {
+            //on insère les données dans la table membre
+            $req = "INSERT INTO congeemploye (idUtilisateur, titreConge, dateConge) VALUES (?,?,?)";
+            $reponse = openDB()->prepare($req);
+            $reponse->execute(array($nom, $raison, $date));
+            $reussi = true;
+        }
 
 
     }
